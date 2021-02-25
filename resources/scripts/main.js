db7b9f0a578a468bc654ef51386cb7545502b220
LieutenantLark <73004856+LieutenantLark@users.noreply.github.com>
2021-02-25 09:51:22 -0800

    Update main.js

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,22 +1,22 @@
 window.onload = function () {
-  localStorage.getItem(document.getElementById('nametxt').value)
-  let name = localStorage.getItem('name')
-  let dev = localStorage.getItem('dev')
-  let savegame = localStorage.getItem('savegame')
-  dev = false
-  console.log("This is an HTML game by:",'Team L\u00B2',"with assistance from renox.")
-  console.log("If buttons do not appear run 'clearStorage();' in the console without the quotes.")
-  console.log('Name:', name, 'Dev mode:', dev)
-  if (name == null || name == '') {
-    document.getElementById('block1').style.display = 'none'
-    document.getElementById('block1').disabled = true
-    document.getElementById('block2').style.display = 'none'
-    document.getElementById('block2').disabled = true
-    document.getElementById('s1').style.display = 'inline-block'
-  }
-  if (localStorage.getItem('name') != null) {
-    document.getElementById('s1').style.display = 'none'
-  }
+   localStorage.getItem(document.getElementById('nametxt').value)
+   let name = localStorage.getItem('name')
+   let dev = localStorage.getItem('dev')
+   let savegame = localStorage.getItem('savegame')
+   dev = false
+   console.log("This is an HTML game by:",'Team L\u00B2',"with assistance from renox.")
+   console.log("If buttons do not appear run 'clearStorage();' in the console without the quotes.")
+   console.log('Name:', name, 'Dev mode:', dev)
+   if (name == null || name == '') {
+      document.getElementById('block1').style.display = 'none'
+      document.getElementById('block1').disabled = true
+      document.getElementById('block2').style.display = 'none'
+      document.getElementById('block2').disabled = true
+      document.getElementById('s1').style.display = 'inline-block'
+   }
+   if (localStorage.getItem('name') != null) {
+      document.getElementById('s1').style.display = 'none'
+   }
 }
 document.getElementById('wname').textContent = 'AMNESIA'
 var snd1 = new Audio('resources/audio/mainambient.mp3')
@@ -24,154 +24,172 @@ var snd2 = new Audio('resources/audio/NGGUU.mp3')
 var snd3 = new Audio('resources/audio/kekw.mp3')
 var snd4 = new Audio('resources/audio/FDB.mp3')
 function bobux () {
-  snd2.play()
-  localStorage.removeItem('name')
+   snd2.play()
+   localStorage.removeItem('name')
 }
 function FDB () {
-  snd4.play()
-  snd4.volume = 1
-  localStorage.removeItem('name')
+   snd4.play()
+   snd4.volume = 1
+   localStorage.removeItem('name')
 }
 function kekw () {
-  snd3.play()
-  localStorage.removeItem('name')
-  setTimeout(
-     function () {
-       window.location.reload()
-     }, 8000)
+   snd3.play()
+   localStorage.removeItem('name')
+   setTimeout(
+      function () {
+         window.location.reload()
+      }, 8000)
 }
 function fadein1 () {
-  setTimeout(
-     function () {
-       document.getElementById('mainG1').style.display = 'block'
-     }, 1000)
+   setTimeout(
+      function () {
+         document.getElementById('mainG1').style.display = 'block'
+      }, 1000)
 }
 function next () {
-  document.getElementById('wrn').style.display = 'none'
-  document.getElementById('scrn1').style.display = 'block'
-  snd1.play()
-  snd1.volume = 0.25
-  snd1.loop = true
-  var targetelement = document.documentElement
-  if (targetelement.requestFullscreen) {
-    targetelement.requestFullscreen()
-  }
-  if (targetelement.mozRequestFullScreen) {
-    targetelement.mozRequestFullScreen()
-  }
-  if (targetelement.msRequestFullscreen) {
-    targetelement.msRequestFullscreen()
-  }
+   document.getElementById('wrn').style.display = 'none'
+   document.getElementById('scrn1').style.display = 'block'
+   snd1.play()
+   snd1.volume = 0.25
+   snd1.loop = true
+   var targetelement = document.documentElement
+   if (targetelement.requestFullscreen) {
+      targetelement.requestFullscreen()
+   }
+   if (targetelement.mozRequestFullScreen) {
+      targetelement.mozRequestFullScreen()
+   }
+   if (targetelement.msRequestFullscreen) {
+      targetelement.msRequestFullscreen()
+   }
 }
 function next1 () {
-  document.getElementById('scrn1').style.display = 'none'
-  document.getElementById('scrn2').style.display = 'block'
+   document.getElementById('scrn1').style.display = 'none'
+   document.getElementById('scrn2').style.display = 'block'
 }
 var textInputElement = document.getElementById('nametxt')
 nameDivElement = document.getElementById('namediv')
 function entername () {
-  setTimeout(
+   setTimeout(
+      function () {
+         localStorage.setItem('name', document.getElementById('nametxt').value)
+         var text = textInputElement.value
+         nameDivElement.innerHTML = text
+         document.getElementById('namediv1').innerHTML = text
+         if (text == 'kekw') {
+            kekw()
+         }
+         if (text == 'dookieballs') {
+            FDB()
+         }
+         if (text == 'LttntLark' || text == 'TeamL') {
+            console.log('DEV MODE ON...clear storage to turn off')
+            // dev mode code here LULw
+            dev = true
+         }
+         if (text == '') {
+            localStorage.removeItem('name')
+            window.location.reload()
+         }
+         if (text == 'bobux') {
+            bobux()
+            setTimeout(
                function () {
-                 localStorage.setItem('name', document.getElementById('nametxt').value)
-                 var text = textInputElement.value
-                 nameDivElement.innerHTML = text
-                 document.getElementById('namediv1').innerHTML = text
-                 if (text == 'kekw') {
-                   kekw()
-                 }
-                 if (text == 'dookieballs') {
-                   FDB()
-                 }
-                 if (text == 'LttntLark' || text == 'TeamL') {
-                   console.log('DEV MODE ON...clear storage to turn off')
-                     // dev mode code here LULw
-                   dev = true
-                 }
-                 if (text == '') {
-                   localStorage.removeItem('name')
-                   window.location.reload()
-                 }
-                 if (text == 'bobux') {
-                   bobux()
-                   setTimeout(
-                        function () {
-                          window.location.reload()
-                        }, 10065)
-                 }
-                 if (text != 'bobux' | 'kekw') {
-                   document.getElementById('sub').style.display = 'block'
-                   document.getElementById('nameshow').style.display = 'block'
-                   document.getElementById('nametxt').disabled = true
-                   document.getElementById('entrnme').disabled = true
-                   document.getElementById('nametxt').style.display = 'none'
-                   document.getElementById('entrnme').style.display = 'none'
-                 }
-               }, 1500)
+                  window.location.reload()
+               }, 10065)
+         }
+         if (text != 'bobux' | 'kekw') {
+            document.getElementById('sub').style.display = 'block'
+            document.getElementById('nameshow').style.display = 'block'
+            document.getElementById('nametxt').disabled = true
+            document.getElementById('entrnme').disabled = true
+            document.getElementById('nametxt').style.display = 'none'
+            document.getElementById('entrnme').style.display = 'none'
+         }
+      }, 1500)
 }
 function startGame () {
-  snd1.pause()
-  document.getElementById('sub').style.display = 'none'
-  document.getElementById('scrn2').style.display = 'none'
-  fadein1()
+   snd1.pause()
+   document.getElementById('sub').style.display = 'none'
+   document.getElementById('scrn2').style.display = 'none'
+   fadein1()
 }
 function startGametrue () {
-  snd1.pause()
-  document.getElementById('scrn1').style.display = 'none'
-  document.getElementById('wrn').style.display = 'none'
-  document.getElementById('mainG1').style.display = 'none'
-  fadein1()
+   snd1.pause()
+   document.getElementById('scrn1').style.display = 'none'
+   document.getElementById('wrn').style.display = 'none'
+   document.getElementById('mainG1').style.display = 'none'
+   fadein1()
 }
 function Continue () {
-  document.getElementById('namediv1').textContent = localStorage.getItem('name')
-  startGametrue()
+   document.getElementById('namediv1').textContent = localStorage.getItem('name')
+   startGametrue()
 }
 function reset () {
-  setTimeout(
-               function () {
-                 document.getElementById('s1').style.display = 'inline-block'
-                 localStorage.removeItem('name')
-                 localStorage.removeItem('savegame')
-                 document.getElementById('block1').style.display = 'none'
-                 document.getElementById('block1').disabled = true
-                 document.getElementById('block2').style.display = 'none'
-                 document.getElementById('block2').disabled = true
-               }, 10)
+   setTimeout(
+      function () {
+         document.getElementById('s1').style.display = 'inline-block'
+         localStorage.removeItem('name')
+         localStorage.removeItem('savegame')
+         document.getElementById('block1').style.display = 'none'
+         document.getElementById('block1').disabled = true
+         document.getElementById('block2').style.display = 'none'
+         document.getElementById('block2').disabled = true
+      }, 10)
 }
 
 function back () {
-  window.location.reload()
+   window.location.reload()
 }
 function showNext1 () {
-  setTimeout(
-               function () {
-                 document.getElementById('sc1').style.display = 'none'
-                 document.getElementById('sc2').style.display = 'block'
-               }, 1000)
+   setTimeout(
+      function () {
+         document.getElementById('sc1').style.display = 'none'
+         document.getElementById('sc2').style.display = 'block'
+      }, 1000)
 }
 function showNext2 () {
-  setTimeout(
-               function () {
-                 document.getElementById('sc2').style.display = 'none'
-                 document.getElementById('sc3').style.display = 'block'
-               }, 1000)
+   setTimeout(
+      function () {
+         document.getElementById('sc2').style.display = 'none'
+         document.getElementById('sc3').style.display = 'block'
+      }, 1000)
 }
 function showNext3 () {
-  setTimeout(
-               function () {
-                 document.getElementById('sc3').style.display = 'none'
-               }, 2000)
-  startgamefr()
+   setTimeout(
+      function () {
+         document.getElementById('sc3').style.display = 'none'
+      }, 2000)
+   startgamefr()
+}
+function showNext4 () {
+   setTimeout(
+      function () {
+         document.getElementById('sub1').style.display = 'none'
+      }, 2000)
+   lev2();
 }
 function startgamefr () {
-  setTimeout(
-               function () {
-                 document.getElementById('game').style.display = 'block'
-                 document.getElementById('mainG1').style.display = 'none'
-               }, 2000)
+   setTimeout(
+      function () {
+         document.getElementById('game').style.display = 'block'
+         document.getElementById('mainG1').style.display = 'none'
+      }, 2000)
 }
-         // game
+function cred1(){
+   setTimeout(
+      function () {
+         document.getElementById('C1').style.display = 'block'
+      }, 2000)
+
+}
+function lev2(){
+document.getElementById('C1').style.display = 'none';
+document.getElementById('Lev2').style.display = 'block';
+}
+// game
 function clearStorage () {
-  localStorage.removeItem('name')
-  localStorage.removeItem('dev')
-  window.location.reload()
+   localStorage.removeItem('name')
+   localStorage.removeItem('dev')
+   window.location.reload()
 }
